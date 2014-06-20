040a621d0b16f344c42b6235025d1a644019c45d
Neil Mithipati <neil.mithipati@gmail.com>
2014-06-20 14:27:38 -0700

    Add initial AJAX request

--- app/assets/javascripts/tumblr_pages.js
+++ app/assets/javascripts/tumblr_pages.js
@@ -1,3 +1,25 @@
 $(function () {
-  var s = $('.user-input').val();
+  $('form').on('submit', function (e) {
+    e.preventDefault();
+    var userName = $('.user-input').val(),
+        url = 'http://'+userName+'.tumblr.com/api/read/json';
+
+    $('.user-input').val("");
+
+    $.ajax({
+      contentType: 'application/json',
+      dataType: 'jsonp',
+      url: url,
+      data: { num: 30 }
+    })
+      .done(function (res, status, req) {
+        console.log(res);
+      })
+      .fail(function (req, status, err) {
+        alert('Could not process your reqeust, there has been an error ' + err);
+      })
+      .always(function () {
+        console.log("Req is complete!");
+      });
+  });
 });
