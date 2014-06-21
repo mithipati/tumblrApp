adae01c455ba9e50553f4d1b9195ea11fcf8c9de
Neil Mithipati <neil.mithipati@gmail.com>
2014-06-20 18:20:11 -0700

    Finalize search funcationality

--- app/assets/javascripts/tumblr_pages.js
+++ app/assets/javascripts/tumblr_pages.js
@@ -17,49 +17,52 @@ $(function () {
       },
       timeout: 3000,
       beforeSend: function () {
+        // Remove error message
         $('.error-message').text('');
+        // Add loading message to DOM
         $('.post-gallery').empty().append('<h1>Loading...</h1>');
       }
     })
       .done(function (res, status, req) {
-        console.log(res.posts);
         _postOutput(res.posts);
       })
       .fail(function (req, status, err) {
         $('.error-message').text('Sorry, could not process your request at this time' + err);
       })
       .always(function () {
-        console.log('Request is complete!');
+        // Remove loading message from DOM
         $('.post-gallery').find('h1').remove();
       });
   });
 
   // Helper Functions
+
+  // Function to determine the type of each post
   function _postOutput(posts) {
     var currentPost;
     for (var i = 0; i < posts.length; i++) {
       currentPost = posts[i];
       switch (currentPost.type) {
         case 'regular':
-          _addPost(currentPost["regular-body"], 'simpleText');
+          _addPost(currentPost["regular-body"], 'simpleText', currentPost["url"]);
           break;
         case 'link':
-          _addPost(currentPost["link-url"], 'link');
+          _addPost(currentPost["link-url"], 'link', currentPost["url"]);
           break;
         case 'quote':
-          _addPost(currentPost["quote-text"], 'simpleText');
+          _addPost(currentPost["quote-text"], 'simpleText', currentPost["url"]);
           break;
         case 'conversation':
-          _addPost(currentPost["conversation-text"], 'simpleText');
+          _addPost(currentPost["conversation-text"], 'simpleText', currentPost["url"]);
           break;
         case 'photo':
-          _addPost(currentPost["photo-url-250"], 'image');
+          _addPost(currentPost["photo-url-250"], 'image', currentPost["url"]);
           break;
         case 'audio':
-          _addPost(currentPost["audio-player"], null);
+          _addPost(currentPost["audio-player"], null, currentPost["url"]);
           break;
         case 'video':
-          _addPost(currentPost["video-player-250"], null);
+          _addPost(currentPost["video-player-250"], null, currentPost["url"]);
           break;
         default:
           $('.error-message').text('Sorry, could not load all posts from this tumblr account.');
@@ -67,11 +70,11 @@ $(function () {
       }
     }
   }
-
-  function _addPost(cont, type) {
-    var baseEl = $('<a />'),
+  // Function to add post elements to DOM
+  function _addPost(cont, type, url) {
+    var baseEl = $('<a />').attr({ href: url, target: '_blank' }),
         mainEl;
-
+    // Determine appropriate HTML tag for each type of post
     if (type === 'simpleText') {
       mainEl = $('<p />').html(cont);
     } else if (type === 'link') {
@@ -81,9 +84,11 @@ $(function () {
     } else {
       mainEl = $('<div />').html(cont);
     }
-
+    // Add styling to each post
     mainEl.addClass('post');
-    $('.post-gallery').append(mainEl);
+    // Wrap each post in an anchor tag that points to the post's url page
+    baseEl.append(mainEl);
+    // Append each post to the gallery
+    $('.post-gallery').append(baseEl);
   }
-
 });
